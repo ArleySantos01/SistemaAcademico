2c9a02bd1d334463d2260ccb8ea71405fc494ffe
Arley <arley.costa@etec.sp.gov.br>
2022-02-21 10:25:54 -0300

    Arley - Inicial

---
+++ SistemaAcademico/SistemaAcademico1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using SistemaAcademico1.model;
+
+namespace SistemaAcademico1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Aluno objAluno = new Aluno(); //Metodo construtor 
+            int x;
+            objAluno.cpf = 123456789;
+            objAluno.nome = "João";
+            objAluno.DtNascimento = new DateTime(1998, 5, 9);
+            Professor joao = new Professor();
+            Console.WriteLine("Hello World!");
+            Console.WriteLine(objAluno.cpf);
+            Console.WriteLine(objAluno.nome);
+            Console.WriteLine(objAluno.DtNascimento);
+
+        }
+    }
+}
---
+++ SistemaAcademico/SistemaAcademico1/model/Pessoa.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaAcademico1.model
+{
+    class Pessoa
+    {
+        //atributos - variaveis de classes 
+        public int cpf;
+        public string nome;
+        public DateTime DtNascimento;
+    }
+}
